9e0ce0ea114e2886fdf04e8e0f49fc873e3c4510
atumar <almustapha.tukur@decagon.dev>
2022-06-10 11:26:04 +0100

    main

--- src/main/java/com/fintech/app/response/BaseResponse.java
+++ src/main/java/com/fintech/app/response/BaseResponse.java
@@ -1,4 +1,20 @@
 package com.fintech.app.response;
 
-public class BaseResponse {
+import lombok.Data;
+
+@Data
+public class BaseResponse<T> {
+
+    private Boolean status;
+    private String message;
+    private T result;
+
+    public BaseResponse() {
+    }
+
+    public BaseResponse(Boolean status, String message, T result) {
+        this.status = status;
+        this.message = message;
+        this.result = result;
+    }
 }
