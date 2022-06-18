4d60d5b66465a975024c7d686fb8b9f3100774f3
atumar <almustapha.tukur@decagon.dev>
2022-06-18 16:26:48 +0100

    testing

--- src/main/java/com/fintech/app/FintechAppApplication.java
+++ src/main/java/com/fintech/app/FintechAppApplication.java
@@ -2,6 +2,8 @@ package com.fintech.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FintechAppApplication {
@@ -9,5 +11,8 @@ public class FintechAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FintechAppApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
