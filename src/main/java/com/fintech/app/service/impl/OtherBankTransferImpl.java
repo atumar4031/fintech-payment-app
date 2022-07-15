e87f4b03fbf4360e8f6723d18ef4a6286a6487dc
atumar <almustapha.tukur@decagon.dev>
2022-07-15 16:52:21 +0100

    working

--- src/main/java/com/fintech/app/service/impl/OtherBankTransferImpl.java
+++ src/main/java/com/fintech/app/service/impl/OtherBankTransferImpl.java
@@ -99,16 +99,17 @@ public class OtherBankTransferImpl implements OtherBankTransferService {
     @Transactional
     @Override
     public BaseResponse<VerifyTransferResponse> verifyTransaction(VerifyTransferRequest verifyTransferRequest) {
-        Long flwRef = verifyTransferRequest.getData().getId();
-        Transfer transfer = transferRepository.findByFlwRef(flwRef)
-                .orElse(null);
+                log.info(verifyTransferRequest.toString());
 
-        if(transfer != null){
-            String status = verifyTransferRequest.getData().getStatus();
-            transfer.setStatus(status);
-            transferRepository.save(transfer);
-        }
-//        log.info(verifyTransferRequest.toString());
+//        Long flwRef = verifyTransferRequest.getData().getId();
+//        Transfer transfer = transferRepository.findByFlwRef(flwRef)
+//                .orElse(null);
+//
+//        if(transfer != null){
+//            String status = verifyTransferRequest.getData().getStatus();
+//            transfer.setStatus(status);
+//            transferRepository.save(transfer);
+//        }
         return new BaseResponse<>(HttpStatus.OK,"verify successfully",null);
     }
 
